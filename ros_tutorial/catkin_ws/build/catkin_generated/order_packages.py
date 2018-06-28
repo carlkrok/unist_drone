7cbccc1122dda6484d1b9dac7f46dc7979756342
Carl <carlkrok@stud.ntnu.no>
2018-06-28 02:55:54 +0000

    ROS turorial

---
+++ ros_tutorial/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/ubuntu/Desktop/unist_drone/ros_tutorial/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/ubuntu/Desktop/unist_drone/ros_tutorial/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/ubuntu/Desktop/unist_drone/ros_tutorial/catkin_ws/devel;/opt/ros/kinetic" != "" else []
