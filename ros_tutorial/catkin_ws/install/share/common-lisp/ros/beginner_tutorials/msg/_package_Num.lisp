352507fb794b65bc49d66bccc1f15d4c5c02669b
Carl <carlkrok@stud.ntnu.no>
2018-06-28 08:13:23 +0000

    Tutorial continued

---
+++ ros_tutorial/catkin_ws/install/share/common-lisp/ros/beginner_tutorials/msg/_package_Num.lisp
@@ -0,0 +1,12 @@
+(cl:in-package beginner_tutorials-msg)
+(cl:export '(NUM-VAL
+          NUM
+          LAST_NAME-VAL
+          LAST_NAME
+          FIRST_NAME-VAL
+          FIRST_NAME
+          AGE-VAL
+          AGE
+          SCORE-VAL
+          SCORE
+))
